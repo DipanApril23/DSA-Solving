cc51aecbb6413ab97efa4e60df93dc8a95a03c71
Dipan Mazumder <dipanmajumder55@gmail.com>
2025-10-26 22:24:35 +0530

    Add files via upload

---
+++ Problem5.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+class Problem5 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        int n = strs.length;
+        if (n == 0) return new ArrayList<>();
+
+        HashMap<String, List<String>> ans = new HashMap<>();
+
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+
+            if (!ans.containsKey(key)) {
+                ans.put(key, new ArrayList<>());
+            }
+            ans.get(key).add(s);
+        }
+
+        return new ArrayList<>(ans.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        Problem5 sol = new Problem5();
+        System.out.println("Group Anagrams: " + sol.groupAnagrams(strs));
+    }
+}
